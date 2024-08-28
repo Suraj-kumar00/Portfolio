b290cd986015ff7c7f3920ccd76199f2c2940490
Suraj <suraj.ukumar.p@gmail.com>
2024-08-28 20:20:54 +0530

    feat: The Hero section is completed.

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -7,12 +7,16 @@ export const HeroOrbit=({
 }: PropsWithChildren<{size: number,rotation: number}>)=>{
     return(
     <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 '>
-      <div className='border border-red-500 size-[800px]' style={{
+      <div 
+      className=''  
+      style={{
         transform: `rotate(${rotation}deg)`,
         height: `${size}px`,
         width: `${size}px`
       }}>
-        <div className='border border-red-500 inline-flex'>{children}</div>
+        <div className=' inline-flex' style={{
+           transform: `rotate(${rotation * -1}deg )`
+        }}>{children}</div>
       </div>
     </div>
     );
